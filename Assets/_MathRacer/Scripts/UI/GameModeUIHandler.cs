9d433db291fb6c46befbdf06a4d27219ef28fb1e
Sinh Ho <sinhvho@gmail.com>
2025-04-14 13:57:32 -0700

    Update GameModeUIHandler - Adjusted to account for memory leaks with listener

--- Assets/_MathRacer/Scripts/UI/GameModeUIHandler.cs
+++ Assets/_MathRacer/Scripts/UI/GameModeUIHandler.cs
@@ -13,12 +13,45 @@ namespace MathRacer
 
         private void Start()
         {
-            addModeBtn.onClick.AddListener(() => { GameManager.Instance.SetGameMode(GameManager.GameMode.Addition); Play(); });
-            subtractModeBtn.onClick.AddListener(() => { GameManager.Instance.SetGameMode(GameManager.GameMode.Subtraction); Play(); });
-            multiplyModeBtn.onClick.AddListener(() => { GameManager.Instance.SetGameMode(GameManager.GameMode.Multiplication); Play(); });
-            divideModeBtn.onClick.AddListener(() => { GameManager.Instance.SetGameMode(GameManager.GameMode.Division); Play(); });
+            addModeBtn?.onClick.AddListener(Addition);
+            subtractModeBtn?.onClick.AddListener(Subtraction);
+            multiplyModeBtn?.onClick.AddListener(Multiplication);
+            divideModeBtn?.onClick.AddListener(Division);
         }
 
+        private void OnDestroy()
+        {
+            addModeBtn?.onClick.RemoveListener(Addition);
+            subtractModeBtn?.onClick.RemoveListener(Subtraction);
+            multiplyModeBtn?.onClick.RemoveListener(Multiplication);
+            divideModeBtn?.onClick.RemoveListener(Division);
+        }
+
+        private void Addition()
+        {
+            GameManager.Instance.SetGameMode(GameManager.GameMode.Addition);
+            Play();
+        }
+
+        private void Subtraction()
+        {
+            GameManager.Instance.SetGameMode(GameManager.GameMode.Subtraction);
+            Play();
+        }
+
+        private void Multiplication()
+        {
+            GameManager.Instance.SetGameMode(GameManager.GameMode.Multiplication);
+            Play();
+        }
+
+        private void Division()
+        {
+            GameManager.Instance.SetGameMode(GameManager.GameMode.Division);
+            Play();
+        }
+
+        // Utilize UniTask or async in the future for loading scenes asynchronously.
         private void Play()
         {
             TransitionController.Instance.PlayTransition(Vector3.zero);
